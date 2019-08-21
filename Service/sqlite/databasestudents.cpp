4b933f08442f7ad72c467f80ebad59267f56455d
Nihil <1009978366@qq.com>
2019-08-21 22:06:36 +0800

    修改json格式

--- Service/sqlite/databasestudents.cpp
+++ Service/sqlite/databasestudents.cpp
@@ -24,8 +24,12 @@ void DatabaseStudents::initialize()
     self->m_database = new SQLiteDatabase(qApp->applicationDirPath() + QString("/database/students.db"));
     self->connect(self->m_database, &SQLiteDatabase::sendMessage, self, &DatabaseStudents::receiveMessage);
 
-    self->db_account.tableName = "account";
+    self->db_account.tableName = "accounts";
     self->db_account.hasAutoIncrementIndex = false;
+    self->db_account.primaryKeys << "code";
     self->db_account.columns << QPair<QString, SQLiteDataType>("code", SQLiteDataType::INTEGER)
             << QPair<QString, SQLiteDataType>("password", SQLiteDataType::TEXT);
+
+    self->db_studentTemplate.hasAutoIncrementIndex = false;
+    self->db_account.primaryKeys << "course_id";
 }
