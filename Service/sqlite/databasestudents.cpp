9014189109a7e137bf93411a77566f69a4d058ee
Nihil <1009978366@qq.com>
2019-08-21 21:41:52 +0800

    更新json格式

--- Service/sqlite/databasestudents.cpp
+++ Service/sqlite/databasestudents.cpp
@@ -1,6 +1,26 @@
-#include "databasestudents.h"
+﻿#include "databasestudents.h"
 
-DatabaseStudents::DatabaseStudents(QObject *parent) : QObject(parent)
+DatabaseStudents *DatabaseStudents::self = new DatabaseStudents;
+
+DatabaseStudents::DatabaseStudents(QObject *parent) :
+    QObject(parent), m_database(nullptr)
 {
 
+}
+
+DatabaseStudents::~DatabaseStudents()
+{
+    if(m_database)
+        delete m_database;
+}
+
+void DatabaseStudents::receiveMessage(MessageType type, QString module, QString message)
+{
+    emit sendMessage(type, module, message);
+}
+
+void DatabaseStudents::initialize()
+{
+    self->m_database = new SQLiteDatabase(qApp->applicationDirPath() + QString("/database/students.db"));
+    self->connect(self->m_database, &SQLiteDatabase::sendMessage, self, &DatabaseStudents::receiveMessage);
 }
--- Service/sqlite/databasestudents.h
+++ Service/sqlite/databasestudents.h
@@ -1,17 +1,30 @@
-#ifndef DATABASESTUDENTS_H
+﻿#ifndef DATABASESTUDENTS_H
 #define DATABASESTUDENTS_H
 
 #include <QObject>
+#include <QApplication>
+#include <QString>
+#include "sqlitedatabase.h"
 
 class DatabaseStudents : public QObject
 {
     Q_OBJECT
 public:
     explicit DatabaseStudents(QObject *parent = nullptr);
+    ~DatabaseStudents();
+
+    static void initialize();
 
 signals:
+    void sendMessage(MessageType type, QString module, QString message);
 
 public slots:
+    void receiveMessage(MessageType type, QString module, QString message);
+
+private:
+    static DatabaseStudents *self;
+    SQLiteDatabase *m_database;
+    SQLiteTable db_studentTemplate;
 };
 
 #endif // DATABASESTUDENTS_H
